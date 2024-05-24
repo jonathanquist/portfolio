036124056ccc1f73a35e7be61ee31a224f704e48
Jonathan <jonathan.quist@outlook.com>
2024-05-24 15:42:11 +0200

    chore: Githubs Action fix

--- src/components/AIChat/AIChatBox.tsx
+++ src/components/AIChat/AIChatBox.tsx
@@ -48,7 +48,7 @@ export default function AIChatBox({ open, onClose }: AIChatBoxProps) {
   return (
     <div
       className={cn(
-        "bottom-0 right-0 z-50 w-full max-w-[500px] p-1 xl:right-36",
+        "bottom-6 right-0 z-50 w-full max-w-[500px] p-1 xl:right-36",
         open ? "fixed" : "hidden",
       )}
     >
